d673ff1e70b7d160ccc9b707a8fccfdcb890a150
Léo Achard <leo.achard@hotmail.fr>
2022-01-04 09:53:15 +0100

    Insert API to Js

--- www/js/main.js
+++ www/js/main.js
@@ -8,5 +8,17 @@ const settings = {
 
 $.ajax(settings).done(function (response) {
 	response = JSON.parse(response);
-	console.log(response);
+	console.log(response.data[0]);
+
+	let currentDate = new Date();
+	console.log(currentDate.toDateString());
+
+	//Injecter dans le html
+	$("h2#city").text(response.data[0].city_name);
+	$("#date").text(currentDate.toDateString());
+	$(".temp h3 span").text(response.data[0].temp.toFixed(1));
+	$("#sunrise").text(response.data[0].sunrise);
+	$("#sunset").text(response.data[0].sunset);
+	$("#precip span").text(response.data[0].precip);
+	$("#wind_spd span").text((response.data[0].wind_spd * 3.6).toFixed(2));
 });
